58f4ba31734aa0c80385f71157a8493dc56ba089
Jacob Ashcraft <00151399@badgermail.snow.edu>
2018-10-31 11:38:27 -0600

    Add project files.

---
+++ SE-4220_HW9-Avalonia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SE_4220_HW9_Avalonia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
